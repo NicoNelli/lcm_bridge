175ceadd402f7317fc80ea88ea8eee88e57698ae
Andrea Nistico <andrea.nistic@gmail.com>
2017-01-16 16:05:08 +0100

    add velocity control support

--- src/bridge_node.cpp
+++ src/bridge_node.cpp
@@ -1,40 +1,33 @@
 #include "lcm/lcm-cpp.hpp"
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/TwistStamped.h"
 #include "lcm_messages/geometry/pose.hpp"
 #include "common/MavState.h"
 #include "common/CallbackHandler.hpp"
 #include <poll.h>
+#include "nav_msgs/Odometry.h"
 
-lcm::LCM handler;
+lcm::LCM handler, handler2;
 geometry::pose lcm_pose;
 CallbackHandler call;
 
-void odometryCallback(geometry_msgs::PoseStamped pose){
+void odometryCallback(nav_msgs::Odometry pose){
 
-    MavState s;
-    s.setPosition((float)pose.pose.position.x,(float)pose.pose.position.y,(float)pose.pose.position.z);
-    s.setOrientation((float)pose.pose.orientation.w,(float)pose.pose.orientation.x,(float)pose.pose.orientation.y,(float)pose.pose.orientation.z);
-    float roll, pitch, yaw;
+    lcm_pose.position[0] =  pose.pose.pose.position.x;
+    lcm_pose.position[1] =  pose.pose.pose.position.y;
+    lcm_pose.position[2] = -pose.pose.pose.position.z;
 
+    lcm_pose.velocity[0] = pose.twist.twist.linear.x;
+    lcm_pose.velocity[1] = pose.twist.twist.linear.y;
+    lcm_pose.velocity[2] = pose.twist.twist.linear.z;
 
-    lcm_pose.position[0] = pose.pose.position.x;
-    lcm_pose.position[1] = pose.pose.position.y;
-    lcm_pose.position[2] = -pose.pose.position.z;
+    lcm_pose.orientation[0] = pose.pose.pose.orientation.w;
+    lcm_pose.orientation[1] = pose.pose.pose.orientation.x;
+    lcm_pose.orientation[2] = pose.pose.pose.orientation.y;
+    lcm_pose.orientation[3] = pose.pose.pose.orientation.z;
 
-    lcm_pose.orientation[0] = pose.pose.orientation.w;
-    lcm_pose.orientation[1] = pose.pose.orientation.x;
-    lcm_pose.orientation[2] = pose.pose.orientation.y;
-    lcm_pose.orientation[3] = pose.pose.orientation.z;
-/*
-    s.orientation(&roll,&pitch,&yaw);
-    std::cout << "************************************************"<< std::endl;
-    std::cout << "Suspected pose POSITION: " << s.getX()<<" " << s.getY() << " " << s.getZ() << std::endl;
-    std::cout << "Suspected pose ATTITUDE: " << roll*RAD2DEG<<" " << pitch*RAD2DEG << " " << yaw*RAD2DEG << std::endl;
-    std::cout << "************************************************"<< std::endl;
-*/
     handler.publish("vision_position_estimate",&lcm_pose);
-
 
 }
 
@@ -45,16 +38,17 @@ int main(int argc, char **argv)
     //ROS helpers
     ros::init(argc, argv, "ros2lcm_bridge");
     ros::NodeHandle n;
-    ros::Subscriber odometry_sub = n.subscribe("/mavros/local_position/pose",1,&odometryCallback);
-    ros::Publisher  pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local",1);
+    ros::Subscriber odometry_sub = n.subscribe("/mavros/local_position/odom",1,&odometryCallback);
+    ros::Publisher  pub  = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local",1);
+    ros::Publisher  pub2 = n.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_velocity/cmd_vel",1);
 
     //LCM stuff
-    lcm::LCM handler;
-    lcm::Subscription *sub2 = handler.subscribe("local_position_sp", &CallbackHandler::positionSetpointCallback, &call);
-    sub2->setQueueCapacity(2);
+
+    lcm::Subscription *sub2 = handler2.subscribe("local_position_sp", &CallbackHandler::positionSetpointCallback, &call);
+    sub2->setQueueCapacity(1);
 
     struct pollfd fds[1];
-    fds[0].fd = handler.getFileno(); // Actual task
+    fds[0].fd = handler2.getFileno(); // Actual task
     fds[0].events = POLLIN;
 
     //main loop
@@ -66,20 +60,35 @@ int main(int argc, char **argv)
 
         if(fds[0].revents & POLLIN){
 
-            geometry_msgs::PoseStamped commandPose;
-            handler.handle();
+            if(call._position_sp.getType() == MavState::type::POSITION) {
+                geometry_msgs::PoseStamped commandPose;
+                handler2.handle();
 
-            commandPose.pose.position.x = call._position_sp.getX();
-            commandPose.pose.position.y = call._position_sp.getY();
-            commandPose.pose.position.z = -call._position_sp.getZ();
+                commandPose.pose.position.x = call._position_sp.getX();
+                commandPose.pose.position.y = call._position_sp.getY();
+                commandPose.pose.position.z = -call._position_sp.getZ();
 
-            commandPose.pose.orientation.x  = call._position_sp.getOrientation().x();
-            commandPose.pose.orientation.y  = call._position_sp.getOrientation().y();
-            commandPose.pose.orientation.z  = call._position_sp.getOrientation().z();
-            commandPose.pose.orientation.w  = call._position_sp.getOrientation().w();
+                commandPose.pose.orientation.x = call._position_sp.getOrientation().x();
+                commandPose.pose.orientation.y = call._position_sp.getOrientation().y();
+                commandPose.pose.orientation.z = call._position_sp.getOrientation().z();
+                commandPose.pose.orientation.w = call._position_sp.getOrientation().w();
 
-            pub.publish(commandPose);
-            std::cout << "command: " << commandPose.pose.position.x << " " << commandPose.pose.position.y << " " <<commandPose.pose.position.z << std::endl;
+
+                std::cout << "command: " << commandPose.pose.position.x << " " << commandPose.pose.position.y << " " <<commandPose.pose.position.z << std::endl;
+                pub.publish(commandPose);
+            }
+            else if(call._position_sp.getType() == MavState::type::VELOCITY) {
+                geometry_msgs::TwistStamped commandPose;
+                handler2.handle();
+
+                commandPose.twist.linear.x =  call._position_sp.getVx();
+                commandPose.twist.linear.y =  call._position_sp.getVy();
+                commandPose.twist.linear.z =  call._position_sp.getVz();
+
+                std::cout << "commandV: " << commandPose.twist.linear.x << " " << commandPose.twist.linear.y << " " <<commandPose.twist.linear.z << std::endl;
+                pub2.publish(commandPose);
+            }
+
             ROS_INFO_ONCE("publish ros command");
 
         }
